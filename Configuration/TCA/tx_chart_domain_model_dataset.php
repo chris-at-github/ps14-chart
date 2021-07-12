eacd83ad26a20e36759ce44384603d4150d60876
Christian Pschorr <c.pschorr@datamints.com>
2021-07-12 22:14:38 +0200

    Implementierung Basis TCA

--- Configuration/TCA/tx_chart_domain_model_dataset.php
+++ Configuration/TCA/tx_chart_domain_model_dataset.php
@@ -1,151 +1,149 @@
 <?php
 return [
-    'ctrl' => [
-        'title' => 'LLL:EXT:chart/Resources/Private/Language/locallang_db.xlf:tx_chart_domain_model_dataset',
-        'label' => 'title',
-        'tstamp' => 'tstamp',
-        'crdate' => 'crdate',
-        'cruser_id' => 'cruser_id',
-        'sortby' => 'sorting',
-        'versioningWS' => true,
-        'languageField' => 'sys_language_uid',
-        'transOrigPointerField' => 'l10n_parent',
-        'transOrigDiffSourceField' => 'l10n_diffsource',
-        'delete' => 'deleted',
-        'enablecolumns' => [
-            'disabled' => 'hidden',
-            'starttime' => 'starttime',
-            'endtime' => 'endtime',
-        ],
-        'searchFields' => 'title',
-        'iconfile' => 'EXT:chart/Resources/Public/Icons/tx_chart_domain_model_dataset.gif'
-    ],
-    'interface' => [
-        'showRecordFieldList' => 'sys_language_uid, l10n_parent, l10n_diffsource, hidden, title, color',
-    ],
-    'types' => [
-        '1' => ['showitem' => 'sys_language_uid, l10n_parent, l10n_diffsource, hidden, title, color, --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access, starttime, endtime'],
-    ],
-    'columns' => [
-        'sys_language_uid' => [
-            'exclude' => true,
-            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.language',
-            'config' => [
-                'type' => 'select',
-                'renderType' => 'selectSingle',
-                'special' => 'languages',
-                'items' => [
-                    [
-                        'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.allLanguages',
-                        -1,
-                        'flags-multiple'
-                    ]
-                ],
-                'default' => 0,
-            ],
-        ],
-        'l10n_parent' => [
-            'displayCond' => 'FIELD:sys_language_uid:>:0',
-            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.l18n_parent',
-            'config' => [
-                'type' => 'select',
-                'renderType' => 'selectSingle',
-                'default' => 0,
-                'items' => [
-                    ['', 0],
-                ],
-                'foreign_table' => 'tx_chart_domain_model_dataset',
-                'foreign_table_where' => 'AND {#tx_chart_domain_model_dataset}.{#pid}=###CURRENT_PID### AND {#tx_chart_domain_model_dataset}.{#sys_language_uid} IN (-1,0)',
-            ],
-        ],
-        'l10n_diffsource' => [
-            'config' => [
-                'type' => 'passthrough',
-            ],
-        ],
-        't3ver_label' => [
-            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.versionLabel',
-            'config' => [
-                'type' => 'input',
-                'size' => 30,
-                'max' => 255,
-            ],
-        ],
-        'hidden' => [
-            'exclude' => true,
-            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.visible',
-            'config' => [
-                'type' => 'check',
-                'renderType' => 'checkboxToggle',
-                'items' => [
-                    [
-                        0 => '',
-                        1 => '',
-                        'invertStateDisplay' => true
-                    ]
-                ],
-            ],
-        ],
-        'starttime' => [
-            'exclude' => true,
-            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.starttime',
-            'config' => [
-                'type' => 'input',
-                'renderType' => 'inputDateTime',
-                'eval' => 'datetime,int',
-                'default' => 0,
-                'behaviour' => [
-                    'allowLanguageSynchronization' => true
-                ]
-            ],
-        ],
-        'endtime' => [
-            'exclude' => true,
-            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.endtime',
-            'config' => [
-                'type' => 'input',
-                'renderType' => 'inputDateTime',
-                'eval' => 'datetime,int',
-                'default' => 0,
-                'range' => [
-                    'upper' => mktime(0, 0, 0, 1, 1, 2038)
-                ],
-                'behaviour' => [
-                    'allowLanguageSynchronization' => true
-                ]
-            ],
-        ],
-        
-    
-    
-        'title' => [
-            'exclude' => true,
-            'label' => 'LLL:EXT:chart/Resources/Private/Language/locallang_db.xlf:tx_chart_domain_model_dataset.title',
-            'config' => [
-                'type' => 'input',
-                'size' => 30,
-                'eval' => 'trim,required'
-            ],
-        ],
-        'color' => [
-            'exclude' => true,
-            'label' => 'LLL:EXT:chart/Resources/Private/Language/locallang_db.xlf:tx_chart_domain_model_dataset.color',
-            'config' => [
-                'type' => 'select',
-                'renderType' => 'selectSingle',
-                'items' => [
-                    ['-- Label --', 0],
-                ],
-                'size' => 1,
-                'maxitems' => 1,
-                'eval' => 'required'
-            ],
-        ],
-    
-        'chart' => [
-            'config' => [
-                'type' => 'passthrough',
-            ],
-        ],
-    ],
+	'ctrl' => [
+		'title' => 'LLL:EXT:chart/Resources/Private/Language/locallang_tca.xlf:tx_chart_domain_model_dataset',
+		'label' => 'title',
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'sortby' => 'sorting',
+		'versioningWS' => true,
+		'languageField' => 'sys_language_uid',
+		'transOrigPointerField' => 'l10n_parent',
+		'transOrigDiffSourceField' => 'l10n_diffsource',
+		'delete' => 'deleted',
+		'enablecolumns' => [
+			'disabled' => 'hidden',
+			'starttime' => 'starttime',
+			'endtime' => 'endtime',
+		],
+		'searchFields' => 'title',
+		'iconfile' => 'EXT:chart/Resources/Public/Icons/tx_chart_domain_model_dataset.gif'
+	],
+	'interface' => [
+		'showRecordFieldList' => 'sys_language_uid, l10n_parent, l10n_diffsource, hidden, title, color',
+	],
+	'types' => [
+		'1' => ['showitem' => 'sys_language_uid, l10n_parent, l10n_diffsource, hidden, title, color, --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access, starttime, endtime'],
+	],
+	'columns' => [
+		'sys_language_uid' => [
+			'exclude' => true,
+			'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.language',
+			'config' => [
+				'type' => 'select',
+				'renderType' => 'selectSingle',
+				'special' => 'languages',
+				'items' => [
+					[
+						'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.allLanguages',
+						-1,
+						'flags-multiple'
+					]
+				],
+				'default' => 0,
+			],
+		],
+		'l10n_parent' => [
+			'displayCond' => 'FIELD:sys_language_uid:>:0',
+			'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.l18n_parent',
+			'config' => [
+				'type' => 'select',
+				'renderType' => 'selectSingle',
+				'default' => 0,
+				'items' => [
+					['', 0],
+				],
+				'foreign_table' => 'tx_chart_domain_model_dataset',
+				'foreign_table_where' => 'AND {#tx_chart_domain_model_dataset}.{#pid}=###CURRENT_PID### AND {#tx_chart_domain_model_dataset}.{#sys_language_uid} IN (-1,0)',
+			],
+		],
+		'l10n_diffsource' => [
+			'config' => [
+				'type' => 'passthrough',
+			],
+		],
+		't3ver_label' => [
+			'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.versionLabel',
+			'config' => [
+				'type' => 'input',
+				'size' => 30,
+				'max' => 255,
+			],
+		],
+		'hidden' => [
+			'exclude' => true,
+			'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.visible',
+			'config' => [
+				'type' => 'check',
+				'renderType' => 'checkboxToggle',
+				'items' => [
+					[
+						0 => '',
+						1 => '',
+						'invertStateDisplay' => true
+					]
+				],
+			],
+		],
+		'starttime' => [
+			'exclude' => true,
+			'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.starttime',
+			'config' => [
+				'type' => 'input',
+				'renderType' => 'inputDateTime',
+				'eval' => 'datetime,int',
+				'default' => 0,
+				'behaviour' => [
+					'allowLanguageSynchronization' => true
+				]
+			],
+		],
+		'endtime' => [
+			'exclude' => true,
+			'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.endtime',
+			'config' => [
+				'type' => 'input',
+				'renderType' => 'inputDateTime',
+				'eval' => 'datetime,int',
+				'default' => 0,
+				'range' => [
+					'upper' => mktime(0, 0, 0, 1, 1, 2038)
+				],
+				'behaviour' => [
+					'allowLanguageSynchronization' => true
+				]
+			],
+		],
+		'title' => [
+			'exclude' => true,
+			'label' => 'LLL:EXT:chart/Resources/Private/Language/locallang_tca.xlf:tx_chart_domain_model_dataset.title',
+			'config' => [
+				'type' => 'input',
+				'size' => 40,
+				'eval' => 'trim,required'
+			],
+		],
+		'color' => [
+			'exclude' => true,
+			'label' => 'LLL:EXT:chart/Resources/Private/Language/locallang_tca.xlf:tx_chart_domain_model_dataset.color',
+			'config' => [
+				'type' => 'select',
+				'renderType' => 'selectSingle',
+				'items' => [
+					['-- Label --', 0],
+					['Color 1', 1],
+					['Color 2', 2],
+				],
+				'size' => 1,
+				'maxitems' => 1,
+				'eval' => 'required'
+			],
+		],
+		'chart' => [
+			'config' => [
+				'type' => 'passthrough',
+			],
+		],
+	],
 ];
--- ext_emconf.php
+++ ext_emconf.php
@@ -11,20 +11,22 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Ps14 Chart',
-    'description' => '',
-    'category' => 'plugin',
-    'author' => 'Christian Pschorr',
-    'author_email' => 'pschorr.christian@gmail.com',
-    'state' => 'alpha',
-    'createDirs' => '',
-    'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '10.4.0-10.4.99',
-        ],
-        'conflicts' => [],
-        'suggests' => [],
-    ],
+	'title' => 'Ps14 Chart',
+	'description' => '',
+	'category' => 'plugin',
+	'author' => 'Christian Pschorr',
+	'author_email' => 'pschorr.christian@gmail.com',
+	'state' => 'alpha',
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'version' => '1.0.0',
+	'constraints' => [
+		'depends' => [
+			'typo3' => '10.4.0-10.4.99',
+			'xo' => '1.0.0-1.10.99',
+			'flux' => '9.5.0-9.5.99',
+		],
+		'conflicts' => [],
+		'suggests' => [],
+	],
 ];
