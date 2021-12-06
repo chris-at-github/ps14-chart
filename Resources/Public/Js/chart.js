9e39bbb003cadad09be291626fa66b5b34554514
Christian Pschorr <c.pschorr@datamints.com>
2021-12-07 00:21:12 +0100

    Sicherheitsabfrage ob ein Canvas-Object zur Verfügung steht

--- Resources/Public/Js/chart.js
+++ Resources/Public/Js/chart.js
@@ -40,122 +40,124 @@
 			};
 
 			const ctx = node.querySelector('.chart--canvas');
-			const chart = new Chart(node.querySelector('.chart--canvas'), {
-				type: 'line',
-				options: {
-					animations: animations,
-					interaction: {
-						intersect: false
-					},
-					maintainAspectRatio: false,
-					plugins : {
-						legend: false,
-						tooltip: {
-							backgroundColor: '#3e3e3e',
-							padding: 12,
-							caretPadding: 10,
-							cornerRadius: 2,
-							usePointStyle: true,
-							boxWidth: 9,
-							boxHeight: 9,
-							titleFont: {
-								family: "'Carlito', 'Calibri', sans-serif",
-								size: 15
-							},
-							bodyFont: {
-								family: "'Carlito', 'Calibri', sans-serif",
-								size: 15
-							},
-							callbacks: {
-								title: function(context) {
-									var title = settings.axis.x.label + ': ' + context[0].label;
-
-									if(settings.axis.x.unit !== '') {
-										title += ' ' + settings.axis.x.unit;
-									}
-
-									return title;
+			if(ctx !== null) {
+				const chart = new Chart(node.querySelector('.chart--canvas'), {
+					type: 'line',
+					options: {
+						animations: animations,
+						interaction: {
+							intersect: false
+						},
+						maintainAspectRatio: false,
+						plugins : {
+							legend: false,
+							tooltip: {
+								backgroundColor: '#3e3e3e',
+								padding: 12,
+								caretPadding: 10,
+								cornerRadius: 2,
+								usePointStyle: true,
+								boxWidth: 9,
+								boxHeight: 9,
+								titleFont: {
+									family: "'Carlito', 'Calibri', sans-serif",
+									size: 15
 								},
-								label: function(context) {
-									var label = '  ' + context.dataset.label || '';
+								bodyFont: {
+									family: "'Carlito', 'Calibri', sans-serif",
+									size: 15
+								},
+								callbacks: {
+									title: function(context) {
+										var title = settings.axis.x.label + ': ' + context[0].label;
 
-									if(label !== '') {
-										label += ': ';
-									}
-
-									if(context.parsed.y !== null) {
-										label += new Intl.NumberFormat('de-DE', {
-											minimumFractionDigits: 2
-										}).format(context.parsed.y);
-
-										if(settings.axis.y.unit !== '') {
-											label += ' ' + settings.axis.y.unit;
+										if(settings.axis.x.unit !== '') {
+											title += ' ' + settings.axis.x.unit;
 										}
-									}
 
-									return label;
+										return title;
+									},
+									label: function(context) {
+										var label = '  ' + context.dataset.label || '';
+
+										if(label !== '') {
+											label += ': ';
+										}
+
+										if(context.parsed.y !== null) {
+											label += new Intl.NumberFormat('de-DE', {
+												minimumFractionDigits: 2
+											}).format(context.parsed.y);
+
+											if(settings.axis.y.unit !== '') {
+												label += ' ' + settings.axis.y.unit;
+											}
+										}
+
+										return label;
+									}
+								}
+							}
+						},
+						scales: {
+							x: {
+								display: true,
+								title: {
+									display: true,
+									text: settings.axis.x.label + ' (' + settings.axis.x.unit + ')',
+									color: '#003865',
+									font: {
+										family: "'Carlito', 'Calibri', sans-serif",
+										size: 18
+									},
+									padding: 14
+								},
+								grid: {
+									borderColor: '#737373',
+									tickColor: '#ececed',
+									color: '#fff'
+								},
+								ticks: {
+									font: {
+										family: "'Carlito', 'Calibri', sans-serif",
+										size: 13
+									},
+								}
+							},
+							y: {
+								beginAtZero: false,
+								display: true,
+								title: {
+									display: true,
+									text: settings.axis.y.label + ' (' + settings.axis.y.unit + ')',
+									color: '#003865',
+									font: {
+										family: "'Carlito', 'Calibri', sans-serif",
+										size: 18
+									},
+									padding: 14
+								},
+								grid: {
+									borderColor: '#737373',
+									tickColor: '#ececed',
+									color: '#ececed'
+								},
+								ticks: {
+									font: {
+										family: "'Carlito', 'Calibri', sans-serif",
+										size: 13
+									},
 								}
 							}
 						}
-					},
-					scales: {
-						x: {
-							display: true,
-							title: {
-								display: true,
-								text: settings.axis.x.label + ' (' + settings.axis.x.unit + ')',
-								color: '#003865',
-								font: {
-									family: "'Carlito', 'Calibri', sans-serif",
-									size: 18
-								},
-								padding: 14
-							},
-							grid: {
-								borderColor: '#737373',
-								tickColor: '#ececed',
-								color: '#fff'
-							},
-							ticks: {
-								font: {
-									family: "'Carlito', 'Calibri', sans-serif",
-									size: 13
-								},
-							}
-						},
-						y: {
-							beginAtZero: false,
-							display: true,
-							title: {
-								display: true,
-								text: settings.axis.y.label + ' (' + settings.axis.y.unit + ')',
-								color: '#003865',
-								font: {
-									family: "'Carlito', 'Calibri', sans-serif",
-									size: 18
-								},
-								padding: 14
-							},
-							grid: {
-								borderColor: '#737373',
-								tickColor: '#ececed',
-								color: '#ececed'
-							},
-							ticks: {
-								font: {
-									family: "'Carlito', 'Calibri', sans-serif",
-									size: 13
-								},
-							}
-						}
 					}
-				}
-			});
+				});
 
-			setTimeout(function() {
-				chart.data.labels = settings.labels;
-				chart.data.datasets = settings.datasets;
-			}, 100);
+				setTimeout(function() {
+					chart.data.labels = settings.labels;
+					chart.data.datasets = settings.datasets;
+				}, 100);
+			}
 		});
 	});
 })();
